b9d6afd9591ffc7341b4777ef1e80a2b8ed70b00
prabaeeeaccet <prabaeeeaccet@gmail.com>
2020-03-15 11:33:29 +0530

    added praba file

---
+++ src/test/java/test/praba.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pages.dresspageobject;
+import pages.homepageobjects;
+import pages.productpageobjects;
+
+public class productpagetest {
+	
+	homepageobjects homepage;
+	dresspageobject dresspage;
+	productpageobjects productpage;
+	
+	public productpagetest(){
+		homepage=new homepageobjects();
+		dresspage=new dresspageobject();
+		productpage=new productpageobjects();
+	}
+	
+	@Test
+	public void tweetsharedispaly(){
+		Assert.assertTrue(homepage.elementfound(productpage.share()));
+		Assert.assertTrue(homepage.elementfound(productpage.twitter()));
+	}
+	@Test
+	public void description(){
+		homepage.dresses().click();
+		dresspage.product().click();
+		Assert.assertTrue(homepage.elementfound(productpage.productdes()));
+	}
+
+}
